e2b89fc315af95be0cbc02ee8c6b9017e8dfb060
Jan <2002jan@gmail.com>
2022-03-17 22:32:22 +0100

    Created function for testing algorithms

--- sortingUtils.h
+++ sortingUtils.h
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <time.h>
+#include "random_data.h"
 
 
 void printArray(int *list, int n);
 
 int checkIfSorted(int *list, int n);
+
+void testAlgorithm(void (*fun)(int *list, int n), int start, int step, int steps, int runs);
 
 /**
  * @brief Prints whole list into console
@@ -32,6 +36,13 @@ void printArray(int *list, int n)
     
 }
 
+/**
+ * @brief Checkes if list on given pointer is sorted
+ * 
+ * @param list 
+ * @param n 
+ * @return int 
+ */
 int checkIfSorted(int *list, int n)
 {
 
@@ -48,4 +59,62 @@ int checkIfSorted(int *list, int n)
     }
     
     return sorted;
+}
+
+/**
+ * @brief Function runs given algorithm {fun} and tests it with given criterias
+ * 
+ * @param fun 
+ * @param start 
+ * @param step 
+ * @param steps 
+ * @param runs 
+ */
+void testAlgorithm(void (*fun)(int *list, int n), int start, int step, int steps, int runs)
+{
+    int *array;
+    int sorted, n;
+    clock_t begin, end;
+
+    for (int i = 0; i < steps; i++)
+    {
+        n = start + step * i;
+        printf("Run %d:", i + 1);
+
+        double timepassed = 0;
+
+        int j;
+
+        for (j = 0; j < runs; j++)
+        {
+            array = (int *)malloc(n * sizeof(int));
+
+            getRandomData(array, n);
+
+            begin = clock();
+            fun(array, n);
+            end = clock();
+
+            sorted = checkIfSorted(array, n);
+
+            if (sorted == 0)
+            {
+                printf(" Failed: \n");
+                printArray(array, n);
+                free(array);
+                break;
+            }
+
+            timepassed += (double)(end - begin) * 1000 / CLOCKS_PER_SEC;
+
+            free(array);
+        }
+        
+        timepassed /= runs;
+
+        if (sorted == 1)
+        {
+            printf(" Passed in %fms\n", timepassed);
+        }
+    }
 }
