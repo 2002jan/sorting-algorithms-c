1a8029c8f12531bfbf51d29e6b3fc910d9d20526
2002jan <2002jan@gmail.com>
2022-04-11 10:49:19 +0200

    Ex. 3

--- main.c
+++ main.c
@@ -15,99 +15,109 @@ int main()
 
     void (*ALG)(int *array, int n);
 
-    printf("\n<----Exercise 1---->\n");
-
-    printf("\nBubble Sort: \n");
-
-    ALG = bubbleSort;
-
-    testAlgorithm(ALG, 20000, 6000, 30, 1, Random, 1, "1", "BS");
-
-    printf("\nHeap Sort: \n");
-
-    ALG = heapSort;
-
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "1", "HS");
-
-    printf("\nCounting Sort: \n");
-
-    ALG = countingSort;
-
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "1", "CS");
-
-    printf("\nShell Sort: \n");
-
-    ALG = shellSort;
-
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "1", "ShS");
-
-    printf("\n<----Exercise 2---->\n");
-
     printf("\nQuick Sort: \n");
 
     ALG = quickSort;
-
-    printf("\nRandom data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "2", "QS");
-
-    printf("\nConstant data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Constant, 1, "2", "QS");
-    
-    printf("\nIncreasing data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Increasing, 1, "2", "QS");
-    
-    printf("\nDescending data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Descending, 1, "2", "QS");
     
     printf("\nAShaped data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, AShaped, 1, "2", "QS");
+    testAlgorithm(ALG, 99999, 1, 3, 5, AShaped, 1, "3", "QS");
     
     printf("\nVShaped data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 1, VShaped, 1, "2", "QS");
+    testAlgorithm(ALG, 99999, 1, 3, 1, VShaped, 1, "3", "QS");
 
-    printf("\nHeap Sort: \n");
+    // printf("\n<----Exercise 1---->\n");
 
-    ALG = heapSort;
+    // printf("\nBubble Sort: \n");
 
-    printf("\nRandom data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "2", "HS");
+    // ALG = bubbleSort;
 
-    printf("\nConstant data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Constant, 1, "2", "HS");
-    
-    printf("\nIncreasing data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Increasing, 1, "2", "HS");
-    
-    printf("\nDescending data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Descending, 1, "2", "HS");
-    
-    printf("\nAShaped data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, AShaped, 1, "2", "HS");
-    
-    printf("\nVShaped data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, VShaped, 1, "2", "HS");
+    // testAlgorithm(ALG, 20000, 6000, 30, 1, Random, 1, "1", "BS");
 
-    printf("\nMerge Sort: \n");
+    // printf("\nHeap Sort: \n");
 
-    ALG = mergeSort;
+    // ALG = heapSort;
 
-    printf("\nRandom data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "2", "MS");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "1", "HS");
 
-    printf("\nConstant data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Constant, 1, "2", "MS");
+    // printf("\nCounting Sort: \n");
+
+    // ALG = countingSort;
+
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "1", "CS");
+
+    // printf("\nShell Sort: \n");
+
+    // ALG = shellSort;
+
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "1", "ShS");
+
+    // printf("\n<----Exercise 2---->\n");
+
+    // printf("\nQuick Sort: \n");
+
+    // ALG = quickSort;
+
+    // printf("\nRandom data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "2", "QS");
+
+    // printf("\nConstant data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Constant, 1, "2", "QS");
     
-    printf("\nIncreasing data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Increasing, 1, "2", "MS");
+    // printf("\nIncreasing data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Increasing, 1, "2", "QS");
     
-    printf("\nDescending data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, Descending, 1, "2", "MS");
+    // printf("\nDescending data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Descending, 1, "2", "QS");
     
-    printf("\nAShaped data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, AShaped, 1, "2", "MS");
+    // printf("\nAShaped data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, AShaped, 1, "2", "QS");
     
-    printf("\nVShaped data: \n");
-    testAlgorithm(ALG, 20000, 6000, 30, 5, VShaped, 1, "2", "MS");
+    // printf("\nVShaped data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 1, VShaped, 1, "2", "QS");
+
+    // printf("\nHeap Sort: \n");
+
+    // ALG = heapSort;
+
+    // printf("\nRandom data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "2", "HS");
+
+    // printf("\nConstant data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Constant, 1, "2", "HS");
+    
+    // printf("\nIncreasing data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Increasing, 1, "2", "HS");
+    
+    // printf("\nDescending data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Descending, 1, "2", "HS");
+    
+    // printf("\nAShaped data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, AShaped, 1, "2", "HS");
+    
+    // printf("\nVShaped data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, VShaped, 1, "2", "HS");
+
+    // printf("\nMerge Sort: \n");
+
+    // ALG = mergeSort;
+
+    // printf("\nRandom data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Random, 1, "2", "MS");
+
+    // printf("\nConstant data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Constant, 1, "2", "MS");
+    
+    // printf("\nIncreasing data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Increasing, 1, "2", "MS");
+    
+    // printf("\nDescending data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, Descending, 1, "2", "MS");
+    
+    // printf("\nAShaped data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, AShaped, 1, "2", "MS");
+    
+    // printf("\nVShaped data: \n");
+    // testAlgorithm(ALG, 20000, 6000, 30, 5, VShaped, 1, "2", "MS");
 
     return 0;
 
